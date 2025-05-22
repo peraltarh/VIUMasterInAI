ac470e7888d87ff61b25cedccc9cb3681c4f19fd
peraltarh <rodrigo.h.peralta@gmail.com>
2025-05-22 20:35:26 +0200

    Added a basic file to test Git connection

---
+++ Master-AI.py
@@ -0,0 +1,11 @@
+def hello_world():
+    print("Hello, world!")
+    print("This is a test of the AI assistant.")
+    a = 5
+    b = 10
+    c = a + b
+    print(f"The sum of {a} and {b} is {c}.")    
+    print("Let's see if it works.")
+
+
+hello_world()
